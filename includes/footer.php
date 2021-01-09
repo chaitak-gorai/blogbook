35c926bac93a17ad49baeb342b9fa65c92cf0b52
chaitak-gorai <77141674+chaitak-gorai@users.noreply.github.com>
2021-01-09 23:59:46 +0530

    Update: Footer title

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
+                    <p>Copyright &copy; Blogbook 2020</p>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
