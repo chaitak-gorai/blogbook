7db79992911205d9354178f61a65647ad06690c0
chaitak-gorai <chaitakgorai@gmail.com>
2021-05-26 19:32:45 +0530

    update

--- includes/db.php
+++ includes/db.php
@@ -1,22 +1,22 @@
 <?php
 //Local SQL database
 //UNcomment it While running in Local Environment
-// $db['db_host']="localhost";
-// $db['db_user']="root";
-// $db['db_pass']="";
-// $db['db_name']="cms";
+$db['db_host'] = "localhost";
+$db['db_user'] = "root";
+$db['db_pass'] = "";
+$db['db_name'] = "cms";
 
 //Remote SQL database
 //Comment it while running in local Environment
-$db['db_host']="remotemysql.com";
-$db['db_user']="DFc7onUv5f";
-$db['db_pass']="7GfIqBKfQm";
-$db['db_name']="DFc7onUv5f";
+// $db['db_host']="remotemysql.com";
+// $db['db_user']="DFc7onUv5f";
+// $db['db_pass']="7GfIqBKfQm";
+// $db['db_name']="DFc7onUv5f";
 
-foreach($db as $key=>$value){
-    
-    define(strtoupper($key),$value);
+foreach ($db as $key => $value) {
+
+    define(strtoupper($key), $value);
 }
 
 
-$connection=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
