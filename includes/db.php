a146d4b7c11533767550599c7bf9696c62df9c07
chaitak-gorai <chaitakgorai@gmail.com>
2021-05-26 15:17:17 +0530

    Added remote SQL connections

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,17 @@
 <?php
-$db['db_host']="localhost";
-$db['db_user']="root";
-$db['db_pass']="";
-$db['db_name']="cms";
+//Local SQL database
+//UNcomment it While running in Local Environment
+// $db['db_host']="localhost";
+// $db['db_user']="root";
+// $db['db_pass']="";
+// $db['db_name']="cms";
+
+//Remote SQL database
+//Comment it while running in local Environment
+$db['db_host']="remotemysql.com";
+$db['db_user']="DFc7onUv5f";
+$db['db_pass']="7GfIqBKfQm";
+$db['db_name']="DFc7onUv5f";
 
 foreach($db as $key=>$value){
     
@@ -11,8 +20,3 @@ foreach($db as $key=>$value){
 
 
 $connection=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-
-
-
-
-?>
