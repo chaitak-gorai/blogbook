0398f947b8bed7f5c23116a5beca1f5107453573
Chaitak Gorai <77141674+chaitak-gorai@users.noreply.github.com>
2021-05-27 23:53:57 +0530

    update db

--- includes/db.php
+++ includes/db.php
@@ -1,17 +1,17 @@
 <?php
 //Local SQL database
 //UNcomment it While running in Local Environment
-$db['db_host'] = "localhost";
-$db['db_user'] = "root";
-$db['db_pass'] = "";
-$db['db_name'] = "cms";
+// $db['db_host'] = "localhost";
+// $db['db_user'] = "root";
+// $db['db_pass'] = "";
+// $db['db_name'] = "cms";
 
 //Remote SQL database
 //Comment it while running in local Environment
-// $db['db_host']="remotemysql.com";
-// $db['db_user']="DFc7onUv5f";
-// $db['db_pass']="7GfIqBKfQm";
-// $db['db_name']="DFc7onUv5f";
+$db['db_host']="remotemysql.com";
+$db['db_user']="DFc7onUv5f";
+$db['db_pass']="7GfIqBKfQm";
+$db['db_name']="DFc7onUv5f";
 
 foreach ($db as $key => $value) {
 
