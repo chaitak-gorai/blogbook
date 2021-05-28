f244e1c3266923880b3404da7acb22b0825acc9c
chaitak-gorai <chaitakgorai@gmail.com>
2021-05-28 22:41:02 +0530

    Form validation
    Validated form using javascript in login and registration page

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,92 +1,129 @@
+<div class="col-md-4">
 
-               
-               
-               
-               <div class="col-md-4">
 
-               
-               
-               
-            
-                <!-- Blog Search Well -->
-                <div class="well">
-                    <h4>Blog Search</h4>
-                    <form action="search.php" method="post"> 
-                       
-                       
-                       <div class="input-group">
-                        <input name="search" type="text" class="form-control">
-                        <span class="input-group-btn">
-                           <button name="submit" class="btn btn-default" type="submit" >
-                                <span class="glyphicon glyphicon-search"></span>
+
+
+
+    <!-- Blog Search Well -->
+    <div class="well">
+        <h4>Blog Search</h4>
+        <form action="search.php" method="post">
+
+
+            <div class="input-group">
+                <input name="search" type="text" class="form-control">
+                <span class="input-group-btn">
+                    <button name="submit" class="btn btn-default" type="submit">
+                        <span class="glyphicon glyphicon-search"></span>
+                    </button>
+                </span>
+            </div>
+        </form>
+        <!-- /.input-group -->
+    </div>
+
+
+    <div class="well logs" id="login">
+        <div class="heads">
+            <h4>Login</h4>
+            <form action="includes/login.php" method="post" id="login_form" onsubmit="return validate_form()">
+
+
+                <div class="form-group">
+                    <input name="username" type="text" class="form-control" placeholder="Enter Username" id="user_name">
+
+                </div>
+
+                <div class="input-group">
+                    <input name="password" type="password" class="form-control" placeholder="Enter Password" id="user_pass">
+                    <span class="input-group-btn">
+                        <button class="btn btn-primary" name="login" type="submit">
+                            Submit
                         </button>
-                        </span>
-                    </div>
-                    </form>
-                    <!-- /.input-group -->
-                </div>
 
-              
-                     <div class="well" id="login">
-                    <h4>Login</h4>
-                    <form action="includes/login.php" method="post"> 
-                       
-                       
-                       <div class="form-group">
-                        <input name="username" type="text" class="form-control" placeholder="Enter Username">
-                        
-                    </div>
-                    
-                        <div class="input-group">
-                        <input name="password" type="password" class="form-control" placeholder="Enter Password">
-                        <span class="input-group-btn">
-                            <button class="btn btn-primary" name="login" type="submit">
-                                Submit
-                            </button>
-                            
-                        </span>
-                        
-                    </div>
-                    </form>
-                    <!-- /.input-group -->
+                    </span>
+
                 </div>
-               
-                
-                 
-                   <!-- Blog Categories Well -->
-                          <?php
-                    
-                    $query="SELECT * FROM categories ";
-                    $select_categories_sidebar=mysqli_query($connection,$query);
-                    ?>    
-                   
-                <div class="well" id="categories">
-                   
-                    <h4>Blog Categories</h4>
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <ul class="list-unstyled">
-                               <?php
-                                
-                                
-                                  
-                    while($row=mysqli_fetch_assoc($select_categories_sidebar)){
-                        $cat_title=$row['cat_title'];
-                        $cat_id=$row['cat_id'];
+            </form>
+
+            <!-- /.input-group -->
+        </div>
+    </div>
+
+
+    <!-- Blog Categories Well -->
+    <?php
+
+    $query = "SELECT * FROM categories ";
+    $select_categories_sidebar = mysqli_query($connection, $query);
+    ?>
+
+    <div class="well " id="categories">
+
+        <h4>Blog Categories</h4>
+        <div class="row">
+            <div class="col-lg-12">
+                <ul class="list-unstyled">
+                    <?php
+
+
+
+                    while ($row = mysqli_fetch_assoc($select_categories_sidebar)) {
+                        $cat_title = $row['cat_title'];
+                        $cat_id = $row['cat_id'];
                         echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
                     }
-                                
-                                ?>
-                            
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
-                      
-                        <!-- /.col-lg-6 -->
-                    </div>
-                    <!-- /.row -->
-                </div>
 
-                <!-- Side Widget Well -->
-              <?php  include "widget.php"?>
+                    ?>
+
+                </ul>
             </div>
+            <!-- /.col-lg-6 -->
+
+            <!-- /.col-lg-6 -->
+        </div>
+        <!-- /.row -->
+    </div>
+
+    <!-- Side Widget Well -->
+    <?php include "widget.php" ?>
+</div>
+<script>
+    function showError(error, alrt) {
+
+        const card = document.querySelector('.logs');
+        const head = document.querySelector('.heads');
+
+
+        const errordiv = document.createElement('div');
+
+        errordiv.className = alrt;
+        errordiv.appendChild(document.createTextNode(error));
+        card.insertBefore(errordiv, head);
+
+        setTimeout(clearError, 3000);
+
+    }
+
+    function clearError() {
+        document.querySelector('.alert').remove();
+    }
+</script>
+<script type='text/javascript'>
+    function validate_form() {
+        // document.getElementById('regis-form').addEventListener('submit', function(event) {
+        //     event.preventDefault();
+        const user = document.getElementById('user_name').value;
+
+        const pass = document.getElementById('user_pass').value;
+
+
+
+        // generate a five digit number for the contact_number variable
+        if (user == "" || pass == "") {
+            showError('Fields Cannot be Empty!', 'alert alert-danger');
+
+            return false;
+        }
+    }
+</script>
--- registration.php
+++ registration.php
@@ -1,44 +1,39 @@
-<?php  include "includes/db.php"; ?>
- <?php  include "includes/header.php"; ?>
+<?php include "includes/db.php"; ?>
+<?php include "includes/header.php"; ?>
 
-<?php 
+<?php
 
 
-if(isset($_POST['submit'])){
-    
-   $username= $_POST['username'];
-    $email=$_POST['email'];
-    $password=$_POST['password'];
-    
-    if(!empty($username)&&!empty($email)&&!empty($password)){
+if (isset($_POST['submit'])) {
 
-    $username=mysqli_real_escape_string($connection,$username);
-    $email=mysqli_real_escape_string($connection,$email);
-    $password=mysqli_real_escape_string($connection,$password);
-    
-    
-    
-    $password=base64_encode($password);
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-//    $password=base64_encode($password);
-// 
-    
-     $query="INSERT INTO users(username,user_email,user_password,user_role) ";
-     $query .="VALUES('{$username}','{$email}','{$password}','new')";
-     $register_user_query=mysqli_query($connection,$query);
-     
-        $message="Registration has been submitted";
-     
- }else{
-        
-        $message="Fields cannot be empty";
-        
-        
+    if (!empty($username) && !empty($email) && !empty($password)) {
+
+        $username = mysqli_real_escape_string($connection, $username);
+        $email = mysqli_real_escape_string($connection, $email);
+        $password = mysqli_real_escape_string($connection, $password);
+
+
+
+        $password = base64_encode($password);
+
+        //    $password=base64_encode($password);
+        // 
+
+        $query = "INSERT INTO users(username,user_email,user_password,user_role) ";
+        $query .= "VALUES('{$username}','{$email}','{$password}','new')";
+        $register_user_query = mysqli_query($connection, $query);
+
+        $message = "    <h6 class='text-center alert alert-success'>Registration has been submitted</h6>";
+    } else {
+        $message = "";
     };
-    }else{
+} else {
     global $message;
-    $message="";
-   
+    $message = "";
 };
 
 
@@ -47,53 +42,97 @@ if(isset($_POST['submit'])){
 
 
 ?>
-   
-   
-   
-   
-   
-   
-    <!-- Navigation -->
-    
-    <?php  include "includes/navigation.php"; ?>
-    
- 
-    <!-- Page Content -->
-    <div class="container">
-    
-<section id="login">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-6 col-xs-offset-3">
-                <div class="form-wrap">
-                <h1>Register</h1>
-                    <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
-                        <h6 class="text-center"><?php echo $message; ?></h6>
-                           <div class="form-group">
-                            <label for="username" class="sr-only">username</label>
-                            <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username">
-                        </div>
-                         <div class="form-group">
-                            <label for="email" class="sr-only">Email</label>
-                            <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
-                        </div>
-                         <div class="form-group">
-                            <label for="password" class="sr-only">Password</label>
-                            <input type="password" name="password" id="key" class="form-control" placeholder="Password">
-                        </div>
-                
-                        <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
-                    </form>
-                 
-                </div>
-            </div> <!-- /.col-xs-12 -->
-        </div> <!-- /.row -->
-    </div> <!-- /.container -->
-</section>
-
-
-        <hr>
 
 
 
-<?php include "includes/footer.php";?>
+
+
+
+<!-- Navigation -->
+
+<?php include "includes/navigation.php"; ?>
+
+
+<!-- Page Content -->
+<div class="container">
+
+    <section id="login">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-6 col-xs-offset-3">
+                    <div class="form-wrap">
+                        <h1 class="head">Register</h1>
+                        <form role="form" action="registration.php" method="post" id="regis-form" autocomplete="off" onsubmit="return validate_form()">
+                            <?php echo $message; ?>
+                            <div class="form-group">
+                                <label for="username" class="sr-only">username</label>
+                                <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username">
+                            </div>
+                            <div class="form-group">
+                                <label for="email" class="sr-only">Email</label>
+                                <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="sr-only">Password</label>
+                                <input type="password" name="password" id="key" class="form-control" placeholder="Password">
+                            </div>
+
+                            <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
+                        </form>
+
+                    </div>
+                </div> <!-- /.col-xs-12 -->
+            </div> <!-- /.row -->
+        </div> <!-- /.container -->
+    </section>
+
+
+    <hr>
+
+    <script>
+        function showError(error, alrt) {
+
+            const card = document.querySelector('.form-wrap');
+            const head = document.querySelector('.head');
+
+
+            const errordiv = document.createElement('div');
+
+            errordiv.className = alrt;
+            errordiv.appendChild(document.createTextNode(error));
+            card.insertBefore(errordiv, head);
+
+            setTimeout(clearError, 3000);
+
+        }
+
+        function clearError() {
+            document.querySelector('.alert').remove();
+        }
+    </script>
+
+    <script type='text/javascript'>
+        function validate_form() {
+            // document.getElementById('regis-form').addEventListener('submit', function(event) {
+            //     event.preventDefault();
+            const user = document.getElementById('username').value;
+            const email = document.getElementById('email').value;
+            const pass = document.getElementById('key').value;
+
+
+
+            // generate a five digit number for the contact_number variable
+            if (user == "" || email == "" || pass == "") {
+                showError('Fields Cannot be Empty!', 'alert alert-danger');
+
+                return false;
+            }
+
+
+
+
+
+        }
+    </script>
+
+    <?php include "includes/footer.php"; ?>
