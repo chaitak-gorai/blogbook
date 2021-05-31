df64780c6b01674bec53efa13e4da5a294306964
KatKava <katryn.kav97@gmail.com>
2021-05-31 12:55:56 +0100

    changed year to 2021 in footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
  <footer>
      <div class="row">
          <div class="col-lg-12">
-             <p>Copyright &copy; Blogbook 2020</p>
+             <p>Copyright &copy; Blogbook 2021</p>
          </div>
          <!-- /.col-lg-12 -->
      </div>
