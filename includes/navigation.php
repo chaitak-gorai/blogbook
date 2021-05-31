386e1fba206029284c7661a3b91fd21d4b00e1c1
chaitak-gorai <chaitakgorai@gmail.com>
2021-05-31 12:00:34 +0530

    Update navbar
    Solved mobile view issue and added collapse

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,56 +1,61 @@
- <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-     <div class="container">
-         <!-- Brand and toggle get grouped for better mobile display -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
 
 
-         <!--
-                <button type="button" value="Category" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
--->
-         <div>
+        <div class="navbar-header">
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-ex2-collapse">
+
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Blogbook</a>
+            <a class="navbar-brand" title="Already Register?" href='index.php#login'>Login</a>
+            <a class="navbar-brand" title="Login First" href='admin/index.php'>Admin</a>
+        </div>
+
+        <div>
 
 
-             <a class="navbar-brand" href="index.php">Blogbook</a>
 
 
-             <div class="container">
 
-                 <a class="navbar-brand" title="Login First" href='admin/index.php'>Admin</a>
-                 <a class="navbar-brand" title="New User?" href='registration.php'>Register</a>
-                 <a class="navbar-brand" title="New User?" href='contact.php'>Contact</a>
-                 <a class="navbar-brand" title="Already Register?" href='index.php#login'>Login</a>
-                 <?php
-                    if (isset($_SESSION['username'])) {
+            <div class="container collapse navbar-collapse navbar-ex2-collapse">
 
-                        if (isset($_GET['p_id'])) {
 
-                            $the_post_id = $_GET['p_id'];
+                <a class="navbar-brand" title="New User?" href='registration.php'>Register</a>
+                <a class="navbar-brand" title="New User?" href='contact.php'>Contact</a>
 
-                            $query = "SELECT * FROM posts WHERE post_id=$the_post_id";
-                            $select_posts = mysqli_query($connection, $query);
-                            while ($row = mysqli_fetch_assoc($select_posts)) {
+                <?php
+                if (isset($_SESSION['username'])) {
 
-                                $author_id = $row['author_id'];
-                            }
-                            $user_role = $_SESSION['user_role'];
-                            $user_id = $_SESSION['user_id'];
-                            if ($user_role == 'admin' || $user_id == $author_id) {
-                                echo "<a class='navbar-brand' href='admin/posts.php?source=edit_post&p_id={$the_post_id}'> Edit Post</a></li>";
-                            }
+                    if (isset($_GET['p_id'])) {
+
+                        $the_post_id = $_GET['p_id'];
+
+                        $query = "SELECT * FROM posts WHERE post_id=$the_post_id";
+                        $select_posts = mysqli_query($connection, $query);
+                        while ($row = mysqli_fetch_assoc($select_posts)) {
+
+                            $author_id = $row['author_id'];
+                        }
+                        $user_role = $_SESSION['user_role'];
+                        $user_id = $_SESSION['user_id'];
+                        if ($user_role == 'admin' || $user_id == $author_id) {
+                            echo "<a class='navbar-brand' href='admin/posts.php?source=edit_post&p_id={$the_post_id}'> Edit Post</a></li>";
                         }
                     }
+                }
 
-                    ?>
+                ?>
 
-             </div>
+            </div>
 
 
-             <!--             Collect the nav links, forms, and other content for toggling -->
-             <!--
+            <!--             Collect the nav links, forms, and other content for toggling -->
+            <!--
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     
@@ -108,8 +113,8 @@
                 </ul>
             </div>
 -->
-             <!--             /.navbar-collapse -->
-         </div>
-     </div>
-     <!--         /.container -->
- </nav>
+            <!--             /.navbar-collapse -->
+        </div>
+    </div>
+    <!--         /.container -->
+</nav>
