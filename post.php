ab14e07f4b5bff0c5cb7cce4b0f2adbbf633eb23
chaitak-gorai <chaitakgorai@gmail.com>
2021-06-02 23:27:30 +0530

    Update post comments

--- post.php
+++ post.php
@@ -88,25 +88,38 @@
 
                         <hr>
                         <!-- Blog Comments -->
+                        <div class="well">
+                            <h4>Comments</h4>
+                            <?php
+                            $query = "SELECT * FROM comments WHERE comment_post_id={$link_post_id} ";
+                            $query .= "AND comment_status='approved' ";
+                            $query .= "ORDER BY comment_id DESC ";
+                            $comment_query1 = mysqli_query($connection, $query);
 
-                        <?php
-                        $query = "SELECT * FROM comments WHERE comment_post_id={$link_post_id} ";
-                        $query .= "AND comment_status='approved' ";
-                        $query .= "ORDER BY comment_id DESC ";
-                        $comment_query1 = mysqli_query($connection, $query);
+                            while ($row = mysqli_fetch_assoc($comment_query1)) {
+                                $comment_date = $row['comment_date'];
+                                $comment_content = $row['comment_content'];
+                                $comment_author = $row['comment_author'];
 
-                        while ($row = mysqli_fetch_assoc($comment_query1)) {
-                            $comment_date = $row['comment_date'];
-                            $comment_content = $row['comment_content'];
-                            $comment_author = $row['comment_author'];
+                            ?>
 
-                        ?>
+                                <div class="media " style="padding-left: 10px;">
 
 
 
-                        <?php } ?>
+                                    <div class="media-body " style="border-radius: 0;">
+                                        <h4 class="media-heading"><?php echo $comment_author; ?>
+                                            <small><?php echo $comment_date; ?></small>
+                                        </h4>
+                                        <?php echo $comment_content; ?>
+                                    </div>
+                                </div>
 
 
+
+                            <?php } ?>
+
+                        </div>
                 <?php }
                 } else {
                     header("Location:index.php");
@@ -116,22 +129,7 @@
                 ?>
 
 
-                <div class="well">
-                    <h4>Comments</h4>
-                    <div class="media">
 
-                        <a class="pull-left" href="#">
-                            <img class="media-object" src="http://placehold.it/64x64" alt="">
-                        </a>
-
-                        <div class="media-body " style="border-radius: 0;">
-                            <h4 class="media-heading"><?php echo $comment_author; ?>
-                                <small><?php echo $comment_date; ?></small>
-                            </h4>
-                            <?php echo $comment_content; ?>
-                        </div>
-                    </div>
-                </div>
 
 
                 <?php
