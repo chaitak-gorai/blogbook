52c3cce1d78974e82b17ee0cb73aea7df577e91e
chaitak-gorai <chaitakgorai@gmail.com>
2021-08-01 22:49:40 +0530

    changed tiny mce api

--- admin/js/script.js
+++ admin/js/script.js
@@ -1,13 +1,13 @@
-  // tinymce.init({
-  //       selector: 'textarea'
-  //     });
+// tinymce.init({
+//       selector: 'textarea'
+//     });
 
-  tinymce.init({
-    selector: 'textarea',
-    plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
-    toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
-    toolbar_mode: 'floating',
-    tinycomments_mode: 'embedded',
-    tinycomments_author: 'Author name',
-    height : "480"
- });
+tinymce.init({
+  selector: 'textarea',
+  plugins: '  autolink lists  media     table  ',
+  toolbar: ' addcomment showcomments   code  table',
+  toolbar_mode: 'floating',
+  tinycomments_mode: 'embedded',
+  tinycomments_author: 'Author name',
+  height: '480',
+});
