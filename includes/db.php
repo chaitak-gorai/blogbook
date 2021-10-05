9a24cef333809634bedd5889fafddf02e20c97e3
Chaitak Gorai <77141674+chaitak-gorai@users.noreply.github.com>
2021-10-05 22:49:58 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -8,15 +8,15 @@
 
 //Remote SQL database
 //Comment it while running in local Environment
-$db['db_host']="remotemysql.com";
-$db['db_user']="DFc7onUv5f";
-$db['db_pass']="7GfIqBKfQm";
-$db['db_name']="DFc7onUv5f";
+// $db['db_host']="remotemysql.com";
+// $db['db_user']="DFc7onUv5f";
+// $db['db_pass']="7GfIqBKfQm";
+// $db['db_name']="DFc7onUv5f";
 
-// $db['db_host']="sql6.freemysqlhosting.net";
-// $db['db_user']="sql6434875";
-// $db['db_pass']="fxHpYP19QZ";
-// $db['db_name']="sql6434875";
+$db['db_host']="sql6.freemysqlhosting.net";
+$db['db_user']="sql6434875";
+$db['db_pass']="fxHpYP19QZ";
+$db['db_name']="sql6434875";
 
 foreach ($db as $key => $value) {
 
