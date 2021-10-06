03c41890e5ea9589ca74ad342c6a02d39c0378c3
Kaique Garcia <kaiquegarciam@gmail.com>
2021-10-06 03:08:04 -0300

    Adding new rendering options to the search page

--- search.php
+++ search.php
@@ -16,97 +16,56 @@
             <div class="col-md-8">
 
                <?php
-                   
-                
+
+
                 if(isset($_POST['submit'])){
                     $search= $_POST['search'];
-                
-                
+
+
                 $query="SELECT *FROM posts WHERE post_tags LIKE '%$search%'";
                 $search_query=mysqli_query($connection, $query);
-                    
+
                     if(!$search_query){
-                        
+
                         die("query faikled".mysql_error($connection));
                     }
-                
-                
+
+
                 $count=mysqli_num_rows($search_query);
-                
+
                if($count==0){
-                   
-                   echo "<h1>no results</h1>";
-                   
-               }else{
-                    
-                    
-                    
+                   include_once __DIR__ . "/includes/empty-post-view.php";
+               } else {
+                    $render = include_once __DIR__ . "/includes/post-view.php";
                     while($row=mysqli_fetch_assoc($search_query)){
-                        $post_title=$row['post_title'];
-                        $post_author=$row['post_author'];
-                        $post_date=$row['post_date'];
-                        $post_image=$row['post_image'];
-                        $post_content=$row['post_content'];
-                       
-                       ?>
-                        
-                        
-                                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
-
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#"><?php echo $post_title;?></a>
-                </h2>                    
-                <p class="lead">
-                    by <a href="index.php"><?php echo $post_author;?></a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date;?></p>
-                <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image;?>" alt="">
-                <hr>
-                <p><?php echo $post_content;?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr> 
-                        
-                        
-                        
-                        
-                       
-                  <?php  } 
+                        $render($row);
+                    }
                }
-                
-                
-                
-                
                 }
-                    ?>
-     
-     
-                
-     
-     
-               
-                        
-                        
-                       
-              
-                
-                
-                
-                
-                
-                
-               
-                   
-                    
-                     
-                      
-                     
-               
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
             </div>
 
@@ -117,9 +76,9 @@
 
         <hr>
 
-      <?php 
+      <?php
 
 include "includes/footer.php";
 
 
-?> 
+?>
