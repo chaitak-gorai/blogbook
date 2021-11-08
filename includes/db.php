c84cebcf8d7cd258c05ffe02a8b9ec9326e4b470
Kristina Pavlicko <91137205+kristonela@users.noreply.github.com>
2021-11-08 22:04:11 +0100

    Like system for posts added

--- includes/db.php
+++ includes/db.php
@@ -1,17 +1,17 @@
 <?php
 //Local SQL database
 //use it if you want to view the database on localhost by importing the database file.
- $db['db_host'] = "localhost";
- $db['db_user'] = "root";
- $db['db_pass'] = "";
- $db['db_name'] = "ess_blogbook";
+// $db['db_host'] = "localhost";
+// $db['db_user'] = "root";
+// $db['db_pass'] = "";
+// $db['db_name'] = "blogbook";
 
 //Remote SQL database
 //use it if you dont need the schema or any manual changes to database
-//$db['db_host']="remotemysql.com";
-//$db['db_user']="dUcPjiYNKu";
-//$db['db_pass']="lGB1H36B1r";
-//$db['db_name']="dUcPjiYNKu";
+$db['db_host']="remotemysql.com";
+$db['db_user']="dUcPjiYNKu";
+$db['db_pass']="lGB1H36B1r";
+$db['db_name']="dUcPjiYNKu";
 
 
 foreach ($db as $key => $value) {
--- includes/like.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$like_query = "UPDATE posts SET post_likes_count=post_likes_count +1 WHERE post_id=$link_post_id";
-$send_query2 = mysqli_query($connection, $like_query);
-
-$query2 = "SELECT * FROM posts WHERE post_id=$link_post_id";
-$like_post_query = mysqli_query($connection, $query2);
